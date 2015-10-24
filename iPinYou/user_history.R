eb644c750cfedc20834cd0bab2aee62aea93d4ba
Wush Wu <wush978@gmail.com>
2015-10-24 16:31:40 +0800

    save user history to sql-lite

---
+++ iPinYou/user_history.R
@@ -0,0 +1,78 @@
+library(methods)
+library(parallel)
+cl <- parallel::makeForkCluster(8)
+library(R.utils)
+library(data.table)
+library(magrittr)
+library(dplyr)
+library(RSQLite)
+db <- dbConnect(dbDriver("SQLite"), "ipinyou.db")
+clusterEvalQ(cl, {
+  library(R.utils)
+  library(data.table)
+  library(magrittr)
+  library(dplyr)
+})
+
+clusterEvalQ(cl, {
+  bidclass <- c(
+    BidID = "character",
+    Timestamp = "character",
+    iPinyouID = "character",
+    UserAgent = "character",
+    IP = "character",
+    Region = "character",
+    City = "character",
+    AdExchange = "character",
+    Domain = "character",
+    URL = "character",
+    AnonymousURLId = "character",
+    AdSlotId = "character",
+    AdSlotWidth = "character",
+    AdSlotHeight = "character",
+    AdSlotVisibility = "character",
+    AdSlotFormat = "character",
+    AdSlotFloorPrice = "numeric",
+    CreativeID = "character",
+    BiddingPrice = "numeric",
+    adid = "character", 
+    usertag = "character"
+  )
+})
+
+src.path <- list(
+  "2nd" = dir("ipinyou.contest.dataset/training2nd", pattern = "^bid.*txt", full.names = TRUE),
+  "3rd" = dir("ipinyou.contest.dataset/training3rd", pattern = "^bid.*txt", full.names = TRUE)
+)
+retval <- list(
+  "2nd" = list(),
+  "3rd" = list()
+)
+
+for(season in names(src.path)) {
+  print(src.path[[season]])
+  retval[[season]] <- parLapply(cl, src.path[[season]], function(path) {
+    dst <- gsub(".bz2", "", path) %>% path.expand()
+    if (!file.exists(dst)) {
+      bunzip2(filename = path, destname = dst)
+    }
+    target <- c("Timestamp", "Domain", "iPinyouID")
+    id <- match(target, names(bidclass))
+    tb <- 
+      fread(dst, sep = "\t", header = FALSE, 
+            select = id,
+            colClasses = as.vector(bidclass),
+            na.strings = "Na",
+            col.names = names(bidclass)[sort(id)],
+            nrows = -1L)
+    tb %>% dplyr::filter(iPinyouID != "")
+  })
+  df <- rbindlist(retval[[season]]) %>% as.data.frame
+  dbWriteTable(db, sprintf("user_history_%s", season), df, 
+               overwrite = TRUE, append = FALSE) %>%
+    stopifnot
+  gc()
+  clusterEvalQ(cl, gc())
+}
+stopCluster(cl)
+dbDisconnect(db)
